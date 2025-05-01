7c56bf3e00586c6a413ce8725232d16ce0cd83d3
DAYALIN1 <00042423@uca.edu.sv>
2025-05-01 14:21:52 -0600

    recursos

---
+++ app/src/main/java/com/frontend/buhoeats/ui/screen/signUp.kt
@@ -0,0 +1,64 @@
+package com.frontend.buhoeats.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.frontend.buhoeats.R
+
+val montserratFontFamily = FontFamily(
+    Font(R.font.montserrat_bold)
+)
+
+@Composable
+fun SignUp(navController: NavController){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF3D405B)
+    ){
+
+        Column(
+            modifier = Modifier.fillMaxSize().padding(32.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                text = "BÚHO EATS",
+                style = TextStyle(
+                    fontFamily = montserratFontFamily,
+                    color = Color.White,
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    shadow = Shadow(
+                        color = Color.White,
+                        offset = Offset(4f,4f),
+                        blurRadius = 12f
+                    )
+
+                )
+            )
+
+
+        }
+    }
+}
--- app/src/main/java/com/frontend/buhoeats/ui/screen/singUp.kt
+++
@@ -1,9 +0,0 @@
-package com.frontend.buhoeats.ui.screen
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-
-@Composable
-fun singUp(navController: NavController){
-
-}
