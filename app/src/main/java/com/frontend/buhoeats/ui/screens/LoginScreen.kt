0438ab5f1d9a3274f7e5e2af61affca77e7c9190
DAYALIN1 <00042423@uca.edu.sv>
2025-04-30 20:38:59 -0600

    boton iniciar sesion

--- app/src/main/java/com/frontend/buhoeats/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/frontend/buhoeats/ui/screens/LoginScreen.kt
@@ -10,7 +10,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -22,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
@@ -131,5 +135,19 @@ fun Login(navControl: NavHostController) {
                     unfocusedContainerColor = Color.White,
                 )
             )
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = { /*menu*/},
+                modifier = Modifier.width(300.dp).height(56.dp).shadow(elevation= 8.dp, shape= RoundedCornerShape(8.dp)),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF06BB0C))
+            ) {
+                Text(
+                    text = "Iniciar sesion ",
+                    style = TextStyle(fontFamily = montserratFontFamily, fontSize = 20.sp, color = Color.White)
+                )
+            }
         }
+
     }}
