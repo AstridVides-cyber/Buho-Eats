56254a102b243da5d62720447c753032984273a3
DAYALIN1 <00042423@uca.edu.sv>
2025-05-01 14:28:19 -0600

    reparando bug en la main

--- app/src/main/java/com/frontend/buhoeats/MainActivity.kt
+++ app/src/main/java/com/frontend/buhoeats/MainActivity.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.frontend.buhoeats.navigation.AppNavigator
-import com.frontend.buhoeats.ui.screen.singUp
 
+import com.frontend.buhoeats.navigation.AppNavigator
 import com.frontend.buhoeats.ui.theme.BuhoEatsTheme
 
 class MainActivity : ComponentActivity() {
