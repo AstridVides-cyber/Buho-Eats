262fb669a4e3f588d9d7023befee680c2e27df45
DAYALIN1 <00042423@uca.edu.sv>
2025-04-30 18:29:50 -0600

    utilizando tipografia

--- app/src/main/java/com/frontend/buhoeats/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/frontend/buhoeats/ui/screens/LoginScreen.kt
@@ -1,2 +1,10 @@
 package com.frontend.buhoeats.ui.screens
 
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.frontend.buhoeats.R
+
+val montserratFontFamily = FontFamily(
+    Font(R.font.montserrat_bold)
+)
+
