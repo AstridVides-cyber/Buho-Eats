0685ad5abb3dec43e5fc5171b1575dde2411c443
DAYALIN1 <00042423@uca.edu.sv>
2025-05-01 17:32:30 -0600

    navegacion

--- app/src/main/java/com/frontend/buhoeats/navigation/AppNavHost.kt
+++ app/src/main/java/com/frontend/buhoeats/navigation/AppNavHost.kt
@@ -6,16 +6,21 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.frontend.buhoeats.ui.screen.SignUp
+//import com.frontend.buhoeats.ui.screen.Login
 
 @Composable
 fun AppNavigator() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "signUp"){
-        composable("signUp"){
+    NavHost(navController = navController, startDestination = "login") {
+        composable("signUp") {
             SignUp(navController)
         }
+        /*composable("login") {
+            Login(navController)
+        }*/
     }
+
 
 
 }
