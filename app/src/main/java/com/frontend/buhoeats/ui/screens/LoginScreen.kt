b5e7306349fef67364f38347224d32a4779126ba
DAYALIN1 <00042423@uca.edu.sv>
2025-04-30 19:43:56 -0600

    agregano fondo y logo

--- app/src/main/java/com/frontend/buhoeats/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/frontend/buhoeats/ui/screens/LoginScreen.kt
@@ -1,12 +1,29 @@
 package com.frontend.buhoeats.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.frontend.buhoeats.R
 
@@ -19,5 +36,31 @@ fun Login(navControl: NavHostController) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF3D405B)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(32.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(24.dp))
 
-}
+            Text(
+                text = "BÚHO EATS",
+                style = TextStyle(
+                    fontFamily = montserratFontFamily,
+                    color = Color.White,
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    shadow = Shadow(
+                        color = Color.White,
+                        offset = Offset(4f, 4f),
+                        blurRadius = 12f
+                    )
+
+                )
+            )
+        }
+    }}
