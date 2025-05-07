2b978bd1adeb4f79785b5450426ef05d6573a130
DAYALIN1 <00042423@uca.edu.sv>
2025-05-06 19:05:22 -0600

    rediseño de validaciones

--- app/src/main/java/com/frontend/buhoeats/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/frontend/buhoeats/ui/screens/LoginScreen.kt
@@ -1,28 +1,13 @@
 package com.frontend.buhoeats.ui.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -30,6 +15,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -49,23 +35,25 @@ fun isValidEmail(email: String): Boolean {
     return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 }
 
-
 @Composable
 fun Login(navControl: NavHostController) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-
     var triedToSubmit by remember { mutableStateOf(false) }
     val isEmailValid = isValidEmail(email)
     val isEmailNotEmpty = email.isNotBlank()
 
+    val context = LocalContext.current
 
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color(0xFF3D405B)
     ) {
         Column(
-            modifier = Modifier.fillMaxSize().padding(32.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp)
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -83,19 +71,21 @@ fun Login(navControl: NavHostController) {
                         offset = Offset(4f, 4f),
                         blurRadius = 12f
                     )
-
                 )
             )
 
-            Spacer( modifier = Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(12.dp))
             Image(
                 painter = painterResource(id = R.drawable.buho),
                 contentDescription = "Logo Búho Eats",
                 modifier = Modifier.size(200.dp)
             )
-            Spacer( modifier = Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(12.dp))
 
-            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
                 Text(
                     text = "Correo:",
                     style = TextStyle(
@@ -111,35 +101,32 @@ fun Login(navControl: NavHostController) {
                 value = email,
                 onValueChange = { email = it },
                 isError = triedToSubmit && (!isEmailNotEmpty || !isEmailValid),
-                placeholder = { Text("ingrese su correo", color = Color.Gray, fontSize = 16.sp ,style = TextStyle(fontFamily = montserratFontFamily)) },
-
+                placeholder = {
+                    Text(
+                        "Ingrese su correo",
+                        color = Color.Gray,
+                        fontSize = 16.sp,
+                        style = TextStyle(fontFamily = montserratFontFamily)
+                    )
+                },
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(16.dp),
                 colors = TextFieldDefaults.colors(
                     focusedContainerColor = Color.White,
                     unfocusedContainerColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Transparent
                 )
             )
-            if (triedToSubmit && !isEmailNotEmpty) {
-                Text(
-                    text = "El campo no debe estar vacío",
-                    color = Color.Red,
-                    fontSize = 14.sp,
-                    style = TextStyle(fontFamily = montserratFontFamily)
-                )
-            }
-          else if (triedToSubmit && !isEmailValid) {
-                Text(
-                    text = "Correo inválido, no es una direccion de correo",
-                    color = Color.Red,
-                    fontSize = 14.sp,
-                    style = TextStyle(fontFamily = montserratFontFamily)
-                )
-            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
                 Text(
                     text = "Contraseña:",
                     style = TextStyle(
@@ -148,50 +135,71 @@ fun Login(navControl: NavHostController) {
                         fontSize = 24.sp
                     )
                 )
-      }
+            }
             Spacer(modifier = Modifier.height(12.dp))
 
             TextField(
                 value = password,
                 onValueChange = { password = it },
-                placeholder = { Text("Ingrese su contraseña", color = Color.Gray, fontSize = 16.sp, style = TextStyle(fontFamily = montserratFontFamily)) },
+                placeholder = {
+                    Text(
+                        "Ingrese su contraseña",
+                        color = Color.Gray,
+                        fontSize = 16.sp,
+                        style = TextStyle(fontFamily = montserratFontFamily)
+                    )
+                },
                 visualTransformation = PasswordVisualTransformation(),
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(16.dp),
                 colors = TextFieldDefaults.colors(
                     focusedContainerColor = Color.White,
                     unfocusedContainerColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    errorIndicatorColor = Color.Transparent
                 )
             )
-            if (triedToSubmit && !isEmailNotEmpty) {
-                Text(
-                    text = "El campo no debe estar vacío",
-                    color = Color.Red,
-                    fontSize = 14.sp,
-                    style = TextStyle(fontFamily = montserratFontFamily)
-                )
-            }
+
             Spacer(modifier = Modifier.height(24.dp))
 
             Button(
-                onClick =  {
+                onClick = {
                     triedToSubmit = true
-                    if (isEmailValid) { }
+                    when {
+                        !isEmailNotEmpty -> {
+                            Toast.makeText(context, "El campo no debe estar vacío", Toast.LENGTH_SHORT).show()
+                        }
+                        !isEmailValid -> {
+                            Toast.makeText(context, "Correo inválido, no es una dirección de correo", Toast.LENGTH_SHORT).show()
+                        }
+                        else -> {
+                            Toast.makeText(context, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+                        }
+                    }
                 },
-                modifier = Modifier.width(300.dp).height(56.dp).shadow(elevation= 8.dp, shape= RoundedCornerShape(8.dp)),
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(56.dp)
+                    .shadow(elevation = 8.dp, shape = RoundedCornerShape(8.dp)),
                 shape = RoundedCornerShape(8.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF06BB0C))
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF06BB0C)),
             ) {
                 Text(
-                    text = "Iniciar sesion ",
-                    style = TextStyle(fontFamily = montserratFontFamily, fontSize = 20.sp, color = Color.White)
+                    text = "Iniciar sesión",
+                    style = TextStyle(
+                        fontFamily = montserratFontFamily,
+                        fontSize = 20.sp,
+                        color = Color.White
+                    )
                 )
             }
 
             Spacer(modifier = Modifier.height(24.dp))
 
             Button(
-                onClick = { /*sesión con Google */ },
+                onClick = { /* Sesión con Google */ },
                 modifier = Modifier
                     .width(300.dp)
                     .height(56.dp)
@@ -200,7 +208,7 @@ fun Login(navControl: NavHostController) {
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color(0xFF4285F4)
                 )
-            ){
+            ) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Center
@@ -219,13 +227,12 @@ fun Login(navControl: NavHostController) {
                         fontSize = 16.sp
                     )
                 }
-
             }
 
             Spacer(modifier = Modifier.height(50.dp))
 
             Text(
-                text = "¿Todavia no tienes cuenta?",
+                text = "¿Todavía no tienes cuenta?",
                 style = TextStyle(
                     fontFamily = montserratFontFamily,
                     color = Color.White,
@@ -236,18 +243,23 @@ fun Login(navControl: NavHostController) {
             Spacer(modifier = Modifier.height(24.dp))
 
             Button(
-                onClick = { /*crear cuenta*/},
-                modifier = Modifier.width(200.dp).height(56.dp).shadow(elevation= 8.dp, shape= RoundedCornerShape(8.dp)),
+                onClick = { /* Crear cuenta */ },
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(56.dp)
+                    .shadow(elevation = 8.dp, shape = RoundedCornerShape(8.dp)),
                 shape = RoundedCornerShape(8.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF06BB0C))
             ) {
                 Text(
                     text = "Crear cuenta",
-                    style = TextStyle(fontFamily = montserratFontFamily, fontSize = 20.sp, color = Color.White)
+                    style = TextStyle(
+                        fontFamily = montserratFontFamily,
+                        fontSize = 20.sp,
+                        color = Color.White
+                    )
                 )
             }
         }
-
-
-
-    }}
+    }
+}
