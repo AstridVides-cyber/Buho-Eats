6d13b1c9d29033e8809adcc437cad72158506ba4
DAYALIN1 <00042423@uca.edu.sv>
2025-05-10 12:28:35 -0600

    creando pantalla para perfil

--- app/src/main/java/com/frontend/buhoeats/ui/screens/profile.kt
+++ app/src/main/java/com/frontend/buhoeats/ui/screens/profile.kt
@@ -1,2 +1,41 @@
 package com.frontend.buhoeats.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import com.frontend.buhoeats.R
+import com.frontend.buhoeats.ui.components.BottomNavigationBar
+import com.frontend.buhoeats.ui.components.TopBar
+
+@Composable
+
+fun Profile(navController: NavController) {
+
+    Scaffold(
+        topBar = {
+            TopBar(
+                showBackIcon = true
+            )
+        },
+        bottomBar = {
+            BottomNavigationBar()
+        }
+    ) { paddingValues ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)) {
+
+            Image(
+                painter = painterResource(id = R.drawable.backgroundlighttheme),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }}}
