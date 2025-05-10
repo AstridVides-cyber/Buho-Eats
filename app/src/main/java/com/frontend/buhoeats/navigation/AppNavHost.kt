bbe3d8d9eab3406bb0e657eef5937edf0eb4ba09
DAYALIN1 <00042423@uca.edu.sv>
2025-05-10 12:27:46 -0600

    creando navegacion

--- app/src/main/java/com/frontend/buhoeats/navigation/AppNavHost.kt
+++ app/src/main/java/com/frontend/buhoeats/navigation/AppNavHost.kt
@@ -1,1 +1,21 @@
 package com.frontend.buhoeats.navigation
+
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.frontend.buhoeats.ui.screens.Profile
+
+@Composable
+fun AppNavigator() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "profile") {
+        composable("profile") {
+            Profile(navController)
+        }
+        }
+
+
+    }
