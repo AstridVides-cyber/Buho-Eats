5714e6639f40246350818e606d600708a47d113a
DAYALIN1 <00042423@uca.edu.sv>
2025-05-10 15:57:53 -0600

    imagenes y titulo

--- app/src/main/java/com/frontend/buhoeats/ui/screens/profile.kt
+++ app/src/main/java/com/frontend/buhoeats/ui/screens/profile.kt
@@ -1,22 +1,63 @@
 package com.frontend.buhoeats.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.IconButton
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.frontend.buhoeats.R
 import com.frontend.buhoeats.ui.components.BottomNavigationBar
 import com.frontend.buhoeats.ui.components.TopBar
 
-@Composable
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.*
+import coil.compose.rememberAsyncImagePainter
 
+
+val montserratFontFamily = FontFamily(
+    Font(R.font.montserrat_bold)
+)
+
+@Composable
 fun Profile(navController: NavController) {
+
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val imagePickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        selectedImageUri = uri
+    }
+
 
     Scaffold(
         topBar = {
@@ -28,9 +69,11 @@ fun Profile(navController: NavController) {
             BottomNavigationBar()
         }
     ) { paddingValues ->
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues)) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
 
             Image(
                 painter = painterResource(id = R.drawable.backgroundlighttheme),
@@ -38,4 +81,68 @@ fun Profile(navController: NavController) {
                 modifier = Modifier.fillMaxSize(),
                 contentScale = ContentScale.Crop
             )
-        }}}
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+            Text(
+                text = "Mi cuenta",
+                style = TextStyle(
+                    fontFamily = montserratFontFamily,
+                    color = Color.Black,
+                    fontSize = 34.sp,
+                    fontWeight = FontWeight.ExtraBold,
+
+                    )
+            )
+        }
+            Spacer(modifier = Modifier.height(16.dp))
+
+                Column(
+                    modifier = Modifier.fillMaxSize().padding(32.dp),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+
+                Spacer(modifier = Modifier.height(48.dp))
+
+                                               Box(contentAlignment = Alignment.BottomEnd) {
+                                if (selectedImageUri != null) {
+                                    Image(
+                                        painter = rememberAsyncImagePainter(selectedImageUri),
+                                        contentDescription = "Imagen de perfil",
+                                        modifier = Modifier
+                                            .size(200.dp)
+                                            .clip(CircleShape),
+                                        contentScale = ContentScale.Crop
+                                    )
+                                } else {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.perfil),
+                                        contentDescription = "Imagen de perfil",
+                                        modifier = Modifier
+                                            .size(200.dp)
+                                            .clip(CircleShape),
+                                        contentScale = ContentScale.Crop
+                                    )
+                                }
+
+                                IconButton(
+                                    onClick = { imagePickerLauncher.launch("image/*") },
+                                    modifier = Modifier
+                                        .offset(x = (-10).dp, y = (-10).dp)
+                                        .size(45.dp)
+                                ) {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.camera),
+                                        contentDescription = "Cambiar imagen"
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+        }
