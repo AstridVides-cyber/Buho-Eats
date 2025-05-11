a9204744c332a08ac4e2de22730a61ea9ebabb4d
DAYALIN1 <00042423@uca.edu.sv>
2025-05-10 19:53:42 -0600

    correccion de navegacioon

--- app/src/main/java/com/frontend/buhoeats/navigation/AppNavHost.kt
+++ app/src/main/java/com/frontend/buhoeats/navigation/AppNavHost.kt
@@ -5,15 +5,18 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.frontend.buhoeats.data.DummyData
+import com.frontend.buhoeats.models.User
 import com.frontend.buhoeats.ui.screens.Profile
 
 @Composable
 fun AppNavigator() {
     val navController = rememberNavController()
+    val user = DummyData.getUser().first()
 
     NavHost(navController = navController, startDestination = "profile") {
         composable("profile") {
-            Profile(navController)
+            Profile(navController, user)
         }
         }
 
