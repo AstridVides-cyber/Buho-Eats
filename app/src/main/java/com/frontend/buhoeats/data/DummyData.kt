b3de0a6a705cbbe0315876881d8c808d20db5d8b
DAYALIN1 <00042423@uca.edu.sv>
2025-05-10 20:34:47 -0600

    cambiando la lista

--- app/src/main/java/com/frontend/buhoeats/data/DummyData.kt
+++ app/src/main/java/com/frontend/buhoeats/data/DummyData.kt
@@ -3,17 +3,23 @@ package com.frontend.buhoeats.data
 import com.frontend.buhoeats.models.User
 
 object DummyData {
+    private var user = User(
+        id = 1,
+        name = "Astrid",
+        lastName = "Vides",
+        imageProfile = "",
+        email = "vides67@gmail.com"
+    )
 
-    fun getUser(): List<User> {
-        return listOf(
-            User(
-                id = 1,
-                name = "Astrid",
-                lastName = "Vides",
-                imageProfile = "",
-                email = "vides67@gmail.com"
-
-            )
-            )
+    fun getUser(): User {
+        return user
     }
+
+    fun updateUser(newUser: User) {
+        user = newUser
+    }
+
+
+
+
 }
