7b180d3a785f9a48fc211c1450e56e645d3dbe2a
DAYALIN1 <00042423@uca.edu.sv>
2025-05-10 19:53:24 -0600

    correccion de navegacioon

--- app/src/main/java/com/frontend/buhoeats/models/Models.kt
+++ app/src/main/java/com/frontend/buhoeats/models/Models.kt
@@ -1,2 +1,12 @@
 package com.frontend.buhoeats.models
 
+import java.io.Serializable
+
+data class User(
+    val id: Int,
+    val name: String,
+    val lastName: String,
+    val email: String,
+    val imageProfile: String,
+
+) : Serializable
