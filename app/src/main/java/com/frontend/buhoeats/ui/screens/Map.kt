10c226d2982ef816c904c8e3b77dbaca6f83e730
DAYALIN1 <00042423@uca.edu.sv>
2025-05-21 11:47:27 -0600

    titulo de restaurantes

--- app/src/main/java/com/frontend/buhoeats/ui/screens/Map.kt
+++ app/src/main/java/com/frontend/buhoeats/ui/screens/Map.kt
@@ -1,22 +1,35 @@
 package com.frontend.buhoeats.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.frontend.buhoeats.R
 import com.frontend.buhoeats.ui.components.BottomNavigationBar
 import com.frontend.buhoeats.ui.components.TopBar
 
 @Composable
 fun Map(onNavigateToProfile: () -> Unit = {},
-        onBack: () -> Unit = {})
-{
+        onBack: () -> Unit = {}) {
+    val scrollState = rememberScrollState()
+
     Scaffold(
         topBar = {
             TopBar(
@@ -33,13 +46,33 @@ fun Map(onNavigateToProfile: () -> Unit = {},
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-
             Image(
                 painter = painterResource(id = R.drawable.backgroundlighttheme),
                 contentDescription = null,
                 modifier = Modifier.fillMaxSize(),
                 contentScale = ContentScale.Crop
             )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White.copy(alpha = 0.8f))
+                    .verticalScroll(scrollState)
+                    .padding(horizontal = 20.dp, vertical =10.dp)
+                    .padding(bottom = 160.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+ {
+                Text(
+                    text = "Restaurantes",
+                    fontSize = 30.sp,
+                    fontFamily = montserratFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 10.dp)
+                )
+            }
         }
     }
 }
