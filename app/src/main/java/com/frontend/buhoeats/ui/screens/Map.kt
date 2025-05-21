ded4f2ba7c362fbaec651063b109187b44897330
DAYALIN1 <00042423@uca.edu.sv>
2025-05-20 19:48:57 -0600

    pantalla map creada

--- app/src/main/java/com/frontend/buhoeats/ui/screens/Map.kt
+++ app/src/main/java/com/frontend/buhoeats/ui/screens/Map.kt
@@ -1,2 +1,45 @@
 package com.frontend.buhoeats.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.frontend.buhoeats.R
+import com.frontend.buhoeats.ui.components.BottomNavigationBar
+import com.frontend.buhoeats.ui.components.TopBar
+
+@Composable
+fun Map(onNavigateToProfile: () -> Unit = {},
+        onBack: () -> Unit = {})
+{
+    Scaffold(
+        topBar = {
+            TopBar(
+                showBackIcon = true,
+                onNavClick = onBack
+            )
+        },
+        bottomBar = {
+            BottomNavigationBar()
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.backgroundlighttheme),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
