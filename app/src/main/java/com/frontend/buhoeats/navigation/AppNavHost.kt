b82fb13596f0b72b6e9a31654f0a6c0e0e80698a
MichiiMaltz11 <00100121@uca.edu.sv>
2025-06-04 12:48:54 -0600

    correcciones de imports

--- app/src/main/java/com/frontend/buhoeats/navigation/AppNavHost.kt
+++ app/src/main/java/com/frontend/buhoeats/navigation/AppNavHost.kt
@@ -3,13 +3,7 @@ package com.frontend.buhoeats.navigation
 import Search
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
