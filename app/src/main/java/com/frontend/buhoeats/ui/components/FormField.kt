6b0d0bbdbbfe62472f58b49cad3362c647274899
DAYALIN1 <00042423@uca.edu.sv>
2025-06-07 19:15:55 -0600

    componentes para la pantallas de edit info

--- app/src/main/java/com/frontend/buhoeats/ui/components/FormField.kt
+++ app/src/main/java/com/frontend/buhoeats/ui/components/FormField.kt
@@ -1,2 +1,48 @@
 package com.frontend.buhoeats.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun FormField(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    isMultiline: Boolean = false
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = label,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            color = Color.Black
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(if (isMultiline) 100.dp else 56.dp),
+            singleLine = !isMultiline,
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Color(0xFFF3EDED),
+                focusedContainerColor = Color(0xFFF3EDED)
+            )
+        )
+    }
+}
+
