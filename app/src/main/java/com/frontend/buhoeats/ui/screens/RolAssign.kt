a540c31418e43a1e0f4cc475bcc67bd43f89e0a0
DAYALIN1 <00042423@uca.edu.sv>
2025-06-17 11:48:13 -0600

    espacioasignado para correo

--- app/src/main/java/com/frontend/buhoeats/ui/screens/RolAssign.kt
+++ app/src/main/java/com/frontend/buhoeats/ui/screens/RolAssign.kt
@@ -1,11 +1,132 @@
 package com.frontend.buhoeats.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.frontend.buhoeats.R
+import com.frontend.buhoeats.ui.components.BottomNavigationBar
+import com.frontend.buhoeats.ui.components.TopBar
 
 @Composable
 fun RolAssign(navController: NavController){
+    var email by remember { mutableStateOf("") }
+    var emailError by remember { mutableStateOf("") }
 
+    Scaffold(
+        topBar = {
+            TopBar(
+                showBackIcon = true,
+                onNavClick = { navController.popBackStack() }
+            )
+        },
+        bottomBar = { BottomNavigationBar(navController) },
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.backgroundlighttheme),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
 
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Text(
+                    text = "Asignar Roles",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = montserratFontFamily,
+                    modifier = Modifier
+                        .padding(top = 50.dp, bottom = 20.dp)
+                )
 
+                Spacer(modifier = Modifier.height(25.dp))
+
+                Text(
+                    "Correo:",
+                    color = Color.Black,
+                    fontSize = 18.sp,
+                    style = TextStyle(fontFamily = montserratFontFamily),
+                    modifier = Modifier
+                        .padding(start = 25.dp, bottom = 8.dp)
+                        .align(Alignment.Start)
+                )
+                OutlinedTextField(
+
+                    value = email,
+                    onValueChange = {
+                        email = it
+                        if (emailError.isNotEmpty()) emailError = ""
+
+                    },
+
+                    placeholder = {
+                        Text(
+                            "Ingrese el correo de el usuario",
+                            color = Color.Gray,
+                            fontSize = 16.sp,
+                            style = TextStyle(fontFamily = montserratFontFamily)
+                        )
+                    },
+                    textStyle = TextStyle(
+                        color = Color.Black,
+                        fontSize = 16.sp,
+                        fontFamily = montserratFontFamily
+                    ),
+                    isError = emailError.isNotEmpty(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+
+                    shape = RoundedCornerShape(16.dp),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color.Black,
+                        unfocusedBorderColor = Color.Black,
+                        disabledBorderColor = Color.Black,
+                        errorBorderColor = Color.Red,
+                        focusedContainerColor = Color(0xFFF3EDED),
+                        unfocusedContainerColor = Color(0xFFF3EDED),
+                        disabledContainerColor = Color(0xFFF3EDED),
+                        errorContainerColor = Color(0xFFF3EDED),
+                        cursorColor = Color.Black
+                    )
+                )
+            }
+        }
+    }
 }
